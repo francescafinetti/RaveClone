20122b68dade3b379e6dddf4cb250e691a6fc90b
francescafinetti <francy.fine@icloud.com>
2026-02-15 16:22:10 +0100

    Initial Commit

---
+++ RaveClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaveClone
+//
+//  Created by Francesca Finetti on 15/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaveClone/RaveCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaveCloneApp.swift
+//  RaveClone
+//
+//  Created by Francesca Finetti on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct RaveCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
